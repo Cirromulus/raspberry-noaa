39cb95b9b2d72ad3940170926f4ace179963edf9
Pascal <Cirromulus@users.noreply.github.com>
2021-02-05 11:56:44 +0100

    using newer librtlsdr lib for improved raw capture

--- install.sh
+++ install.sh
@@ -104,11 +104,11 @@ fi
 if [ -e /usr/local/bin/rtl_fm ]; then
     log_done "rtl-sdr was already installed"
 else
-    log_running "Installing rtl-sdr from osmocom..."
+    log_running "Installing rtl-sdr from librtlsdr..."
     (
         cd /tmp/
-        git clone https://github.com/osmocom/rtl-sdr.git
-        cd rtl-sdr/
+        git clone https://github.com/librtlsdr/librtlsdr.git
+        cd librtlsdr/
         mkdir build
         cd build
         cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
